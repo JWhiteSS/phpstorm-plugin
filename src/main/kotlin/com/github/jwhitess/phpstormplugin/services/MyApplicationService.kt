748c7ecc482b33e5d689db1c3b03a637f5649ab7
GitHub Action <action@github.com>
2020-12-29 19:34:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/jwhitess/phpstormplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jwhitess/phpstormplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jwhitess.phpstormplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jwhitess.phpstormplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jwhitess/phpstormplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jwhitess/phpstormplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jwhitess.phpstormplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jwhitess.phpstormplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
